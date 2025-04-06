3f849b82a0bf7c2f69bf8c1efb78b026d6803e84
hundodayso <solder-elision.0y@icloud.com>
2025-04-06 21:11:25 +0100

    1

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
-response = requests.get(url)
-
+response = requests.get(URL)
 web_page = response.text
 
 soup = BeautifulSoup(web_page, "html.parser")
 
-headlines = soup.find_all()
+films = [headline.getText() for headline in soup.find_all(name="h3", class_="title")]
+films.reverse()
+print(films)
+
+with open("films.txt", 'w', encoding='utf-8') as f:
+    for film in films:
+        f.write(film)
+        f.write("\n")
+
 
