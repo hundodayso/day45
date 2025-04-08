48a4ccfbec3e6baf8cab3a6f9b90093f214def7b
hundodayso <solder-elision.0y@icloud.com>
2025-04-08 22:47:41 +0100

    1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-date = str(input("What year do you want to travel to? Type the date in format YYYY-MM-DD: "))
-url  = f"https://www.billboard.com/charts/hot-100/{date}/"
+#date = str(input("What year do you want to travel to? Type the date in format YYYY-MM-DD: "))
+#url  = f"https://www.billboard.com/charts/hot-100/{date}/"
 
-#url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
+url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
 
 print(url)
 
@@ -15,12 +15,18 @@ print(url)
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
 response = requests.get(url, headers=header)
-print(response)
+#print(response)
 
 billboard_webpage = response.text
 #print(billboard_webpage)
 
 soup = BeautifulSoup(billboard_webpage, 'html.parser')
 
-songs = [song_name.getText() for song_name in soup.find_all(name="h3", id="title-of-a-story")]
+song_list = soup.select("li ul li h3")
+
+songs = [song_name.getText().strip() for song_name in song_list]
 print(songs)
+
+#songs = [song_name.getText().strip() for song_name in soup.find_all(name="h3", class_=lambda value: value and value.startswith("c-title"))]
+
+#songs = [song_name.getText() for song_name in soup.find_all(name="h3", id="title-of-a-story")]
