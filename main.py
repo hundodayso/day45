e9147b1fe7923385f9f671a3488a3fe1019dc111
hundodayso <solder-elision.0y@icloud.com>
2025-04-10 23:15:25 +0100

    1

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
 from bs4 import BeautifulSoup
 import requests
+import keys
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+SP_CLIENT_ID = keys.client_id
+SP_CLIENT_SECRET = keys.client_secret
+SP_REDIRECT = keys.client_uri
 
 #date = str(input("What year do you want to travel to? Type the date in format YYYY-MM-DD: "))
 #url  = f"https://www.billboard.com/charts/hot-100/{date}/"
 
-url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
 
-print(url)
+scope = "user-read-currently-playing"
 
-# header = {
-#     "User=Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0"
-# }
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SP_CLIENT_ID,
+                                               client_secret=SP_CLIENT_SECRET, redirect_uri=SP_REDIRECT, scope=scope))
 
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
 
-response = requests.get(url, headers=header)
-#print(response)
+user = sp.currently_playing()
+print(user)
 
-billboard_webpage = response.text
-#print(billboard_webpage)
 
-soup = BeautifulSoup(billboard_webpage, 'html.parser')
 
-song_list = soup.select("li ul li h3")
 
-songs = [song_name.getText().strip() for song_name in song_list]
-print(songs)
+# url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
+#
+#
+# # header = {
+# #     "User=Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0"
+# # }
+#
+# header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+#
+# response = requests.get(url, headers=header)
+# #print(response)
+#
+# billboard_webpage = response.text
+# #print(billboard_webpage)
+#
+# soup = BeautifulSoup(billboard_webpage, 'html.parser')
+#
+# song_list = soup.select("li ul li h3")
+#
+# songs = [song_name.getText().strip() for song_name in song_list]
+# print(songs)
 
 #songs = [song_name.getText().strip() for song_name in soup.find_all(name="h3", class_=lambda value: value and value.startswith("c-title"))]
 
