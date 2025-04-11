a4a5a09913e194990aaeddf7f2ba5c3d73e09610
hundodayso <solder-elision.0y@icloud.com>
2025-04-11 20:52:49 +0100

    1

--- main.py
+++ main.py
@@ -1,55 +1,66 @@
 from bs4 import BeautifulSoup
 import requests
-import keys
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
+from spotipy.oauth2 import SpotifyOAuth
+import keys
+
+spotify_ep = "https://api.spotify.com/v1/search"
 SP_CLIENT_ID = keys.client_id
 SP_CLIENT_SECRET = keys.client_secret
-SP_REDIRECT = keys.client_uri
+SP_REDIRECT = keys.redirect
+
+scope = "playlist-modify-private"
 
 #date = str(input("What year do you want to travel to? Type the date in format YYYY-MM-DD: "))
+#date = date.strip('-')
 #url  = f"https://www.billboard.com/charts/hot-100/{date}/"
+date = "2012-10-10"
+url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
+response = requests.get(url, headers=header)
 
-scope = "user-read-currently-playing"
+billboard_webpage = response.text
+
+soup = BeautifulSoup(billboard_webpage, 'html.parser')
+
+song_list = soup.select("li ul li h3")
+
+songs = [song_name.getText().strip() for song_name in song_list]
+print(songs)
+
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SP_CLIENT_ID,
-                                               client_secret=SP_CLIENT_SECRET, redirect_uri=SP_REDIRECT, scope=scope))
+                                               client_secret=SP_CLIENT_SECRET,
+                                               redirect_uri=SP_REDIRECT,
+                                               scope=scope
+                                               ))
 
-# results = sp.current_user_saved_tracks()
-# for idx, item in enumerate(results['items']):
-#     track = item['track']
-#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+#
 
-user = sp.currently_playing()
+
+####Example Song Response#
+# song_response = sp.search('https://api.spotify.com/v1/search?q=remaster%252'
+#                           '0track%3ADoxy%2520artist%3AMiles%2520Davis&type=album&market=UK&limit=2')
+
+track_uris = []
+
+for song in songs:
+    q = f"track:{song} year:{date[:4]}"
+    try:
+        song_response = sp.search(q=q, limit=1, type="track", market="GB", offset=0)
+        print(song_response)
+        track_uris.append(song_response["tracks"]["items"][0]["uri"])
+    except IndexError:
+        print(f"Song {song} not available")
+
+print(track_uris)
+playlist_name = f"Billboardd 100 songs of {date}"
+user = sp.current_user()["id"]
 print(user)
+new_playlist = sp.user_playlist_create(user=user, name=playlist_name, public=False, collaborative=False, description=f"Playlist of the Billboard 100 for {date}")
+sp.playlist_add_items(playlist_id=new_playlist["id"], items=track_uris)
 
 
 
-
-# url = f"https://www.billboard.com/charts/hot-100/2012-10-10/"
-#
-#
-# # header = {
-# #     "User=Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0"
-# # }
-#
-# header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
-#
-# response = requests.get(url, headers=header)
-# #print(response)
-#
-# billboard_webpage = response.text
-# #print(billboard_webpage)
-#
-# soup = BeautifulSoup(billboard_webpage, 'html.parser')
-#
-# song_list = soup.select("li ul li h3")
-#
-# songs = [song_name.getText().strip() for song_name in song_list]
-# print(songs)
-
-#songs = [song_name.getText().strip() for song_name in soup.find_all(name="h3", class_=lambda value: value and value.startswith("c-title"))]
-
-#songs = [song_name.getText() for song_name in soup.find_all(name="h3", id="title-of-a-story")]
