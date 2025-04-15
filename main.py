cc7d984a99320c1ba6e1ab666d80b2e48b0fd930
hundodayso <solder-elision.0y@icloud.com>
2025-04-15 22:51:23 +0100

    1

--- main.py
+++ main.py
@@ -3,8 +3,30 @@ from selenium.webdriver.common.by import By
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+chrome_options.add_experimental_option("detach", False)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.amazon.co.uk")
+driver.get("https://www.python.org/")
+
+element = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[3]/div[2]')
+event_dates = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li a")
+
+
+events = []
+for i in range(len(event_names)):
+   event_date = event_dates[i].text.split("T")[0]
+   event_name = event_names[i].text
+   event = [event_date, event_name]
+   events.append(event)
+
+print(events)
+
+
+
+
+
+
+
+
