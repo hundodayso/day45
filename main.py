4a3aa2a4f26dd8d86990eb0d5ef50855c93caa43
hundodayso <solder-elision.0y@icloud.com>
2025-04-17 22:48:41 +0100

    1

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
+import time
+URL = "https://orteil.dashnet.org/experiments/cookie/"
+TIME_TO_PLAY = 30
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", False)
+chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.python.org/")
+driver.get(URL)
+game_on = True
 
-element = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[3]/div[2]')
-event_dates = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li time")
-event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li a")
 
-events = {}
-print(len(event_names))
-for i in range(len(event_names)):
-    events[i] = {
-        "time": event_dates[i].text.split("T")[0],
-        "name": event_dates[i].text
-    }
+cookie = driver.find_element(By.ID, value="cookie")
+start_time = time.time()
+old_time = time.time()
 
-print(events)
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in store]
+#print(item_ids)
 
+
+#while loop to do bits
+while game_on:
+    cookie.click()
+
+    money = driver.find_element(By.ID, value="money")
+    time_now = time.time()
+
+    if time_now - old_time >= 5:
+
+        store_prices = driver.find_elements(By.CSS_SELECTOR, value='#store b')
+
+
+        old_time = time.time() #set the old to the time now
+    if time_now - start_time >= TIME_TO_PLAY: #check if exceeded play time
+        print("Game Ending...")
+        game_on = False
+
+
+# for i in range(10):
+#     cookie.click()
+#
+#     print(money.text)
---
+++ main_trial_day48.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", False)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.python.org/")
+
+
+element = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[3]/div[2]')
+event_dates = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget ul li a")
+
+events = {}
+print(len(event_names))
+for i in range(len(event_names)):
+    events[i] = {
+        "time": event_dates[i].text.split("T")[0],
+        "name": event_dates[i].text
+    }
+
+print(events)
+
