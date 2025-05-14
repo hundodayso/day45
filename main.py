af38e8e5bb4c71425132e9d9766ad0c73cad7ffb
hundodayso <solder-elision.0y@icloud.com>
2025-05-14 21:24:03 +0100

    1

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ url = "https://appbrewery.github.io/Zillow-Clone/"
 webpage = requests.get(url).text
 soup = BeautifulSoup(webpage, "html.parser")
 
-cards = soup.find_all(name="a", class_="StyledPropertyCardDataArea-anchor")
-for card in cards:
 
-    print(card.get("href"))
+cards = soup.find_all(name="a", class_="StyledPropertyCardDataArea-anchor")
+
+links = [link.get("href") for link in cards]
+print(links)
+#for card in cards:
+
+prices = soup.find(class_="PropertyCardWrapper__StyledPriceLine")
+print(prices.get_text())
+
