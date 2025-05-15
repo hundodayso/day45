2a1ded553346934aaed5a56d8d38540bea2d350f
hundodayso <solder-elision.0y@icloud.com>
2025-05-15 22:42:18 +0100

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 url = "https://appbrewery.github.io/Zillow-Clone/"
@@ -14,6 +15,9 @@ links = [link.get("href") for link in cards]
 print(links)
 #for card in cards:
 
-prices = soup.find(class_="PropertyCardWrapper__StyledPriceLine")
-print(prices.get_text())
 
+card_prices = soup.find_all(class_="PropertyCardWrapper__StyledPriceLine")
+prices_as_int = [int("".join(re.findall(r'\d+', property.get_text()))) for property in card_prices]
+prices_as_string = [re.sub(r"[+ /mo]", "", property.get_text()) for property in card_prices]
+print(prices_as_int)
+print(prices_as_string)
