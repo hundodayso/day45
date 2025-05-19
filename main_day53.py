337a15849ef08ef35b44e55cf28c4adaa57fd46f
hundodayso <solder-elision.0y@icloud.com>
2025-05-19 20:22:25 +0100

    1

--- main_day53.py
+++ main_day53.py
@@ -24,15 +24,12 @@ links = [link.get("href") for link in cards]
 print(links)
 #for card in cards:
 
-
 card_prices = soup.find_all(class_="PropertyCardWrapper__StyledPriceLine")
 #prices_as_int = [int("".join(re.findall(r'\d+', property.get_text()))) for property in card_prices]
 #prices_as_string = [re.sub(r"[+ /mo|\1bd]", "", property.get_text()) for property in card_prices]
 prices_as_string = [re.split(r"\+|/", property.get_text())[0] for property in card_prices]
 #print(prices_as_int)
 print(prices_as_string)
-
-
 
 card_addresses = soup.find_all(name="address")
 addresses = [address.get_text().replace("|","").strip() for address in card_addresses]
@@ -42,10 +39,40 @@ print(addresses)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+
+
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(form_url)
-driver.implicitly_wait(15)
-address = driver.find_element(By.CSS_SELECTOR, value=".rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd")
+#driver.implicitly_wait(15)
 
-#address.click()
-address.send_keys("hello my name is")
+length_of_loop = len(links)
+
+for i in range(length_of_loop):
+
+    input_address = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    #address = driver.find_element(By.CSS_SELECTOR, value="#whsOnd zHQkBf")
+    if addresses[i]:
+        input_address.send_keys(addresses[i])
+    else:
+        input_address.send_keys("Placeholder")
+
+    input_price = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    if prices_as_string[i]:
+        input_price.send_keys(prices_as_string[i])
+    else:
+        input_price.send_keys("Placeholder")
+
+
+    input_link = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input")
+    if links[i]:
+        input_link.send_keys(links[i])
+    else:
+        input_link.send_keys("Placeholder")
+
+    submit_button = driver.find_element(By.XPATH, value="/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
+
+    submit_button.click()
+    driver.implicitly_wait(3)
+    submit_another_button = driver.find_element(By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div/div[4]/a")
+    submit_another_button.click()
+    driver.implicitly_wait(10)
